54887f9ceebd6be5581bcde246ac8abb918c4f6b
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-11-25 00:45:50 -0600

    Removed commented-out code

--- example/empty_example.cpp
+++ example/empty_example.cpp
@@ -23,8 +23,4 @@ int main()
   for (auto& thread : threads) {
     thread.join();
   }
-
-  // for (int i = 0; i < 1E6; ++i) {
-  //   log.log(binary_log::level::info, "Hello logger: msg number {}", i);
-  // }
 }
