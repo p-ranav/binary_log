03232cbc22a518da3af7c8f96293ad0b734782ba
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-11-27 18:32:32 -0600

    Updated unpacker build to include argparse header

--- tools/unpacker/unpacker.cpp
+++ tools/unpacker/unpacker.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+#include <argparse.hpp>
+
 int main(int argc, char* argv[])
 {
-  std::cout << "Hello World!\n";
+  argparse::ArgumentParser program("unpacker");
+  program.add_argument("-i", "--index-file").help("Index file").required();
+  program.add_argument("-l", "--log-file").help("Log file").required();
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::runtime_error& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1);
+  }
+
+  auto index_file_path = program.get<std::string>("-i");
+  auto log_file_path = program.get<std::string>("-l");
+
+  std::cout << index_file_path << std::endl;
+  std::cout << log_file_path << std::endl;
 }
