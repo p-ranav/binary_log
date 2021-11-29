5fffc0a8a20bb33d232bfbef528613ad1748bc37
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-11-28 23:24:46 -0600

    Updated example

--- example/example.cpp
+++ example/example.cpp
@@ -1,21 +1,9 @@
-#include <chrono>
-#include <iostream>
-
 #include <binary_log/binary_log.hpp>
 
 int main()
 {
   binary_log::binary_log log("log.out");
 
-  constexpr auto how_many = 1E6;
-  auto start = std::chrono::high_resolution_clock::now();
-
-  for (auto i = 0; i < how_many; ++i)
-    BINARY_LOG(log, "Hello World");
-
-  auto end = std::chrono::high_resolution_clock::now();
-  std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end
-                                                                     - start)
-                   .count()
-            << " us\n";
+  for (uint32_t i = 0; i < 1E6; ++i)
+    BINARY_LOG(log, "Hello logger, msg number: {}", i);
 }
