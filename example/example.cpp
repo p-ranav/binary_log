4d854e1f35491708a0b20ca32950c17611c4116f
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-11-28 20:26:21 -0600

    Reverted simple example

--- example/example.cpp
+++ example/example.cpp
@@ -4,15 +4,4 @@ int main()
 {
   binary_log::binary_log log("log.out");
   BINARY_LOG(log, "Hello World");
-
-  for (std::size_t i = 0; i < 5; ++i) {
-    BINARY_LOG(log, "binary_log is {}", binary_log::constant("awesome"));
-  }
-
-  BINARY_LOG(log, "Dynamic data: {} {}", 123, "abc");
-
-  for (std::size_t i = 0; i < 12345; ++i) {
-    BINARY_LOG(log,
-               "binary_log performs run-length encoding on some log calls");
-  }
 }
