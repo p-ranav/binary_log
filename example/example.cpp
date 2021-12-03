23bbd97e047632f7bb7153be52a9d5c4f863d563
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-12-03 08:25:03 -0600

    Reverted change to example, need to measure full time including destruction of the logger

--- example/example.cpp
+++ example/example.cpp
@@ -4,9 +4,9 @@
 
 int main()
 {
-  binary_log::binary_log log("log.out");
   auto start = std::chrono::high_resolution_clock::now();
-  {    
+  {
+    binary_log::binary_log log("log.out");
     for (uint32_t i = 0; i < 1E6; ++i) {
       BINARY_LOG(log, "Hello logger, msg number: {}", i);
     }
