7ecdd94e2646c810b817d9f06f586f111e78d728
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-12-03 20:29:22 -0600

    Added million integers test to benchmark

--- benchmark/binary_log_benchmark.cpp
+++ benchmark/binary_log_benchmark.cpp
@@ -57,6 +57,31 @@ static void BM_binary_log_constants(benchmark::State& state)
   remove("log.out.index");
 }
 
+static void BM_binary_log_bool(benchmark::State& state)
+{
+  // Perform setup here
+  binary_log::binary_log log("log.out");
+
+  for (auto _ : state) {
+    // This code gets timed
+    BINARY_LOG(log, "{}", true);
+  }
+
+  state.counters["Logs/s"] =
+      benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+  const auto size_of_each_log_entry = (1 + sizeof(bool));
+  state.counters["Bytes/s"] = benchmark::Counter(
+      state.iterations() * size_of_each_log_entry, benchmark::Counter::kIsRate);
+
+  state.counters["Latency"] = benchmark::Counter(
+      state.iterations(),
+      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+
+  remove("log.out");
+  remove("log.out.index");
+}
+
 static void BM_binary_log_integer(benchmark::State& state)
 {
   // Perform setup here
@@ -71,7 +96,32 @@ static void BM_binary_log_integer(benchmark::State& state)
       benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
 
   // 42 can be stored as a uint8_t, so binary_log will just write 1 byte
-  const auto size_of_each_log_entry = (1 + 1 + sizeof(uint8_t));
+  const auto size_of_each_log_entry = (1 + 1 + sizeof(int8_t));
+  state.counters["Bytes/s"] = benchmark::Counter(
+      state.iterations() * size_of_each_log_entry, benchmark::Counter::kIsRate);
+
+  state.counters["Latency"] = benchmark::Counter(
+      state.iterations(),
+      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+
+  remove("log.out");
+  remove("log.out.index");
+}
+
+static void BM_binary_log_float(benchmark::State& state)
+{
+  // Perform setup here
+  binary_log::binary_log log("log.out");
+
+  for (auto _ : state) {
+    // This code gets timed
+    BINARY_LOG(log, "{}", 2.71828f);
+  }
+
+  state.counters["Logs/s"] =
+      benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+  const auto size_of_each_log_entry = (1 + sizeof(float));
   state.counters["Bytes/s"] = benchmark::Counter(
       state.iterations() * size_of_each_log_entry, benchmark::Counter::kIsRate);
 
@@ -135,11 +185,41 @@ static void BM_binary_log_string(benchmark::State& state)
   remove("log.out.index");
 }
 
+static void BM_binary_log_million_integers(benchmark::State& state)
+{
+  // Perform setup here
+  binary_log::binary_log log("log2.out");
+
+  for (auto _ : state) {
+    // This code gets timed
+    for (int i = 0; i < 1E6; ++i) {
+      BINARY_LOG(log, "{}", i);
+    }
+  }
+
+  state.counters["Logs/s"] =
+      benchmark::Counter(1E6 * state.iterations(), benchmark::Counter::kIsRate);
+
+  const auto output_log_file_size = 5934336 * state.iterations();
+  state.counters["Bytes/s"] =
+      benchmark::Counter(output_log_file_size, benchmark::Counter::kIsRate);
+
+  state.counters["Latency"] = benchmark::Counter(
+      1E6 * state.iterations(),
+      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+
+  remove("log2.out");
+  remove("log2.out.index");
+}
+
 BENCHMARK(BM_binary_log_static_string);
 BENCHMARK(BM_binary_log_constants);
+BENCHMARK(BM_binary_log_bool);
 BENCHMARK(BM_binary_log_integer);
+BENCHMARK(BM_binary_log_float);
 BENCHMARK(BM_binary_log_double);
 BENCHMARK(BM_binary_log_string);
+BENCHMARK(BM_binary_log_million_integers)->Unit(benchmark::kMillisecond);
 
 // Run the benchmark
 BENCHMARK_MAIN();
