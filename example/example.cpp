7d4c56a27c977ea86a4452a8268500b698fd35b1
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-12-04 19:12:13 -0600

    Removed chrono from example

--- example/example.cpp
+++ example/example.cpp
@@ -1,18 +1,10 @@
-#include <chrono>
-
 #include <binary_log/binary_log.hpp>
 
 int main()
 {
   binary_log::binary_log log("log.out");
 
-  auto start = std::chrono::high_resolution_clock::now();
-  for (uint64_t i = 0; i < 1E6; ++i) {
+  for (uint64_t i = 0; i < 1E9; ++i) {
     BINARY_LOG(log, "Hello logger, msg number: {}", i);
   }
-  auto end = std::chrono::high_resolution_clock::now();
-  auto diff_ns =
-      std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  std::cout << "Time    : " << diff_ns << " ns" << std::endl;
-  std::cout << "Latency : " << diff_ns / 1E9 << " ns" << std::endl;
 }
