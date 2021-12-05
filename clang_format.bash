04ae8e453482aa9f0159e469df7cec38a0cb95a5
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-12-04 21:14:03 -0600

    Added examples/ directory to kepe more than one example

--- clang_format.bash
+++ clang_format.bash
@@ -1,1 +1,1 @@
-clang-format-11 -i benchmark/*.cpp include/binary_log/*.hpp include/binary_log/detail/*.hpp example/*.cpp tools/unpacker/*.cpp tools/unpacker/*.hpp
+clang-format-11 -i benchmark/*.cpp include/binary_log/*.hpp include/binary_log/detail/*.hpp examples/billion_integers/*.cpp tools/unpacker/*.cpp tools/unpacker/*.hpp
