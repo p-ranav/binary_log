716ed3380c7652448cd2f8570b6ba5de8f0727d5
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2021-12-04 21:06:44 -0600

    Updated benchmarks with static int, float, double, and string inputs

--- benchmark/binary_log_benchmark.cpp
+++ benchmark/binary_log_benchmark.cpp
@@ -4,17 +4,84 @@
 #include <benchmark/benchmark.h>
 #include <binary_log/binary_log.hpp>
 
-static void BM_binary_log_random_bool(benchmark::State& state)
+template<typename T>
+static void BM_binary_log_static_integer(benchmark::State& state)
 {
-  std::random_device dev;
-  std::mt19937 rng(dev());
-  std::uniform_int_distribution<uint8_t> distr(0, 1);
   {
     binary_log::binary_log log("log.out");
 
     for (auto _ : state) {
       // This code gets timed
-      BINARY_LOG(log, "{}", bool(distr(rng)));
+      BINARY_LOG(log, "{}", T(state.range(0)));
+    }
+  }
+
+  state.counters["Logs/s"] =
+      benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+  state.counters["Latency"] = benchmark::Counter(
+      state.iterations(),
+      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+
+  remove("log.out");
+  remove("log.out.index");
+  remove("log.out.runlength");
+}
+
+static void BM_binary_log_static_float(benchmark::State& state)
+{
+  {
+    binary_log::binary_log log("log.out");
+
+    for (auto _ : state) {
+      // This code gets timed
+      BINARY_LOG(log, "{}", 2.7182818284590452353602874713527f);
+    }
+  }
+
+  state.counters["Logs/s"] =
+      benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+  state.counters["Latency"] = benchmark::Counter(
+      state.iterations(),
+      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+
+  remove("log.out");
+  remove("log.out.index");
+  remove("log.out.runlength");
+}
+
+static void BM_binary_log_static_double(benchmark::State& state)
+{
+  {
+    binary_log::binary_log log("log.out");
+
+    for (auto _ : state) {
+      // This code gets timed
+      BINARY_LOG(log, "{}", 3.1415926535897932384626433832795);
+    }
+  }
+
+  state.counters["Logs/s"] =
+      benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
+
+  state.counters["Latency"] = benchmark::Counter(
+      state.iterations(),
+      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
+
+  remove("log.out");
+  remove("log.out.index");
+  remove("log.out.runlength");
+}
+
+static void BM_binary_log_static_string(benchmark::State& state)
+{
+  {
+    binary_log::binary_log log("log.out");
+
+    for (auto _ : state) {
+      // This code gets timed
+      BINARY_LOG(log, "Hello {}", "World");
     }
   }
 
@@ -37,15 +104,6 @@ static void BM_binary_log_random_integer(benchmark::State& state)
   std::mt19937 rng(dev());
   std::uniform_int_distribution<T> distr(std::numeric_limits<T>::min(),
                                          std::numeric_limits<T>::max());
-
-  {
-    binary_log::binary_log log("log.out");
-
-    for (auto _ : state) {
-      // This code gets timed
-      BINARY_LOG(log, "{}", distr(rng));
-    }
-  }
 
   {
     binary_log::binary_log log("log.out");
@@ -96,47 +154,19 @@ static void BM_binary_log_random_real(benchmark::State& state)
   remove("log.out.runlength");
 }
 
-std::string generate_random_string(const int len)
-{
-  static const char alphanum[] =
-      "0123456789"
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      "abcdefghijklmnopqrstuvwxyz";
-  std::string tmp_s;
-  tmp_s.reserve(len);
-
-  for (int i = 0; i < len; ++i) {
-    tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
-  }
-
-  return tmp_s;
-}
-
-template<std::size_t N>
-static void BM_binary_log_random_string(benchmark::State& state)
-{
-  {
-    binary_log::binary_log log("log.out");
-
-    for (auto _ : state) {
-      // This code gets timed
-      BINARY_LOG(log, "{}", generate_random_string(N));
-    }
-  }
-
-  state.counters["Logs/s"] =
-      benchmark::Counter(state.iterations(), benchmark::Counter::kIsRate);
-
-  state.counters["Latency"] = benchmark::Counter(
-      state.iterations(),
-      benchmark::Counter::kIsRate | benchmark::Counter::kInvert);
-
-  remove("log.out");
-  remove("log.out.index");
-  remove("log.out.runlength");
-}
-
-BENCHMARK(BM_binary_log_random_bool);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, uint8_t)->Arg(42);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, uint16_t)->Arg(395);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, uint32_t)->Arg(3123456789);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, uint64_t)
+    ->Arg(9876543123456789);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, int8_t)->Arg(-42);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, int16_t)->Arg(-395);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, int32_t)->Arg(-123456789);
+BENCHMARK_TEMPLATE(BM_binary_log_static_integer, int64_t)
+    ->Arg(-9876543123456789);
+BENCHMARK(BM_binary_log_static_float);
+BENCHMARK(BM_binary_log_static_double);
+BENCHMARK(BM_binary_log_static_string);
 BENCHMARK_TEMPLATE(BM_binary_log_random_integer, uint8_t);
 BENCHMARK_TEMPLATE(BM_binary_log_random_integer, uint16_t);
 BENCHMARK_TEMPLATE(BM_binary_log_random_integer, uint32_t);
@@ -147,12 +177,6 @@ BENCHMARK_TEMPLATE(BM_binary_log_random_integer, int32_t);
 BENCHMARK_TEMPLATE(BM_binary_log_random_integer, int64_t);
 BENCHMARK_TEMPLATE(BM_binary_log_random_real, float);
 BENCHMARK_TEMPLATE(BM_binary_log_random_real, double);
-BENCHMARK_TEMPLATE(BM_binary_log_random_string, 2);
-BENCHMARK_TEMPLATE(BM_binary_log_random_string, 4);
-BENCHMARK_TEMPLATE(BM_binary_log_random_string, 8);
-BENCHMARK_TEMPLATE(BM_binary_log_random_string, 16);
-BENCHMARK_TEMPLATE(BM_binary_log_random_string, 32);
-BENCHMARK_TEMPLATE(BM_binary_log_random_string, 64);
 
 // Run the benchmark
 BENCHMARK_MAIN();
